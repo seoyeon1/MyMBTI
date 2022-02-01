4b4006332f2ec984ad56ffc6bfb998d31248d639
seoyeon1 <okyeon1@naver.com>
2022-02-02 00:17:57 +0900

    공유하기 오류 수정

--- MyMBTI-main/start/js/share.js
+++ MyMBTI-main/start/js/share.js
@@ -2,7 +2,7 @@ const url= "https://12-mbti-lover.netlify.app/";
 
 
 function setShare(){
-    var resultImg = document.querySelector('resultImg');
+    var resultImg = document.querySelector('#resultImg');
     var resultAlt= resultImg.firstElementChild.alt;
     const shareTitle = '십이간지 연애유형 결과';
     const shareDesc = infoList[resultAlt].name;
@@ -12,22 +12,22 @@ function setShare(){
     Kakao.Link.sendDefault({
         objectType: 'feed',
         content: {
-        title: shareTitle,
-        description: shareDesc,
-        imageUrl:
-            shareImg,
-        link: {
-            mobileWebUrl: shareURL,
-            webUrl: shareURL
-        },
+            title: shareTitle,
+            description: shareDesc,
+            imageUrl:
+                shareImg,
+            link: {
+                mobileWebUrl: shareURL,
+                webUrl: shareURL
+            },
         },
         
         buttons: [
         {
-            title: '결과 확인하기',
+            title: '결과확인하기',
             link: {
             mobileWebUrl: shareURL,
-            webUrl : shareURL
+            webUrl : shareURL,
             },
         },
 
